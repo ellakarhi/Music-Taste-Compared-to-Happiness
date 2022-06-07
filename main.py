b4dc478d42677f10c681b9f95b65dc4025d40955
Ella Karhi <100384206+ellakarhi@users.noreply.github.com>
2022-06-07 14:21:07 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+file = open("Music Taste in Relation to Happiness TXT.csv")
+
+person = ('Music Taste in Relation to Happiness TXT.csv')
+pop = "Pop"
+print (person[2])
+for pop in file:
+  #variables
+  x = 0
+  y = 0
+  x = x + int(person[2])
+  y = y +1
+  print(x)
+
+
+
